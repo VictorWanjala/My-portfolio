ee65ff338b1b6fc32e01d8a66b7611b64975127f
Victor <wanjala94vic@gmail.com>
2024-01-19 09:53:36 +0300

    updating projects

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -5,28 +5,28 @@ import { GitHub } from '@mui/icons-material';
 import '../styles/ProjectDisplay.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function ProjectDisplay() {
-    const { id } = useParams();
-    const project = projectList[id];
+  const { id } = useParams();
+  const project = projectList[id];
 
-    return (
-        <div className='project'>
-            <h1>{project.name}</h1>
-            <img src={project.image} alt={project.name} />
-            <p><b>Technologies:</b> {project.technologies}</p>
+  return (
+    <div className="container project">
+      <h1 className="display-4">{project.name}</h1>
+      <img src={project.image} alt={project.name} className="img-fluid rounded" />
+      <p className="lead"><b>Technologies:</b> {project.technologies}</p>
 
-            {project.liveDemo && (
-              <a href={project.liveDemo} target="_blank" rel="noopener noreferrer" className="demo-link">
-                  Live Demo
-              </a>
-            )}
+      {project.liveDemo && (
+        <a href={project.liveDemo} target="_blank" rel="noopener noreferrer" className="btn btn-primary mr-2">
+          Live Demo
+        </a>
+      )}
 
-            <a href={project.github} target="_blank" rel="noopener noreferrer">
-                <GitHub />
-            </a>
-        </div>
-    );
+      <a href={project.github} target="_blank" rel="noopener noreferrer" className="btn btn-dark">
+        <GitHub /> GitHub
+      </a>
+    </div>
+  );
 }
 
 export default ProjectDisplay;
+

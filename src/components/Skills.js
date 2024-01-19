f39f97372741ee56e3e5d510dfd00b8ebd67fdec
Victor <wanjala94vic@gmail.com>
2024-01-19 09:39:19 +0300

    updating projects

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,35 +1,38 @@
-import React from 'react'
-import '../styles/Skills.css'
+import React from 'react';
+import '../styles/Skills.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Skills() {
   return (
-    <div className="skills-container">
-        <div className="skills-card">
-            <h2>Programming Languages</h2>
-                <ul>
-                    <li>JavaScript</li>
-                    <li>Python</li>
-                    <li>Kotlin</li>
-                    <li>PHP</li>
-                </ul>
-            <h2>Frameworks</h2>
-                    <ul>
-                        <li>ReactJS</li>
-                        <li>Node.js</li>
-                        <li>Flask</li>
-                    </ul>
-                <h2>Web Technologies</h2>
-                    <ul>
-                        <li>AJAX</li>
-                        <li>REST API</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                    </ul>
-        </div>
+    <div className="container skills-container">
+      <div className="card skills-card">
+        <div className="card-body">
+          <h2 className="card-title">Programming Languages</h2>
+          <ul className="list-group">
+            <li className="list-group-item">JavaScript</li>
+            <li className="list-group-item">Python</li>
+            <li className="list-group-item">Kotlin</li>
+            <li className="list-group-item">PHP</li>
+          </ul>
 
+          <h2 className="card-title mt-4">Frameworks</h2>
+          <ul className="list-group">
+            <li className="list-group-item">ReactJS</li>
+            <li className="list-group-item">Node.js</li>
+            <li className="list-group-item">Flask</li>
+          </ul>
+
+          <h2 className="card-title mt-4">Web Technologies</h2>
+          <ul className="list-group">
+            <li className="list-group-item">AJAX</li>
+            <li className="list-group-item">REST API</li>
+            <li className="list-group-item">HTML</li>
+            <li className="list-group-item">CSS</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
